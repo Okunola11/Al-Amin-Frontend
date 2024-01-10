112be4fbad33b5f34bfbd314c789304d5227d81a
Abdulwasiu <abdayoade@gmail.com>
2024-01-10 07:48:54 +0100

    Added NewUserForm

---
+++ src/features/users/NewUserForm.js
@@ -0,0 +1,203 @@
+import { useState, useEffect, useRef } from "react";
+import { ROLES } from "../../config/roles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faCheck,
+  faInfoCircle,
+  faInfo,
+} from "@fortawesome/free-solid-svg-icons";
+
+const username_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{4,23}$/;
+const password_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{4,23}$/;
+
+const NewUserForm = () => {
+  const usernameRef = useRef();
+
+  const [username, setUsername] = useState("");
+  const [validUsername, setValidUsername] = useState(false);
+  const [usernameFocus, setUsernameFocus] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatchPassword, setValidMatchPassword] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [roles, setRoles] = useState(["Employee"]);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const validUser = username_REGEX.test(username);
+    setValidUsername(validUser);
+  }, []);
+
+  useEffect(() => {
+    const ValidPwd = password_REGEX.test(password);
+    setValidPassword(ValidPwd);
+    const matchPwd = password === matchPassword;
+    setValidMatchPassword(matchPwd);
+  }, []);
+
+  const onUsernameChange = (e) => setUsername(e.target.value);
+  const onPasswordChange = (e) => setPassword(e.target.value);
+  const onMatchChange = (e) => setMatchPassword(e.target.value);
+
+  const rolesOption = Object.values(ROLES).map((role) => (
+    <option key={role} value={role}>
+      {role}
+    </option>
+  ));
+
+  const onRolesChange = (e) => {
+    const options = [...e.target.selectedOptions];
+    const value = options.map((option) => option.value);
+    setRoles(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const content = (
+    <section className="newUser">
+      <p className={isError ? "errmsg" : "offscreen"}>{error?.data.message}</p>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <h2>Add New Employee</h2>
+
+        <label className="form__label" htmlFor="username" aria-live="assertive">
+          Username
+          <span className={validUsername ? "valid" : "hide"}>
+            {<FontAwesomeIcon icon={faCheck} />}
+          </span>
+          <span className={!validUsername && username ? "invalid" : "hide"}>
+            {<FontAwesomeIcon icon={faTimes} />}
+          </span>
+        </label>
+        <input
+          className="form__input"
+          type="text"
+          id="username"
+          ref={usernameRef}
+          value={username}
+          onChange={onUsernameChange}
+          required
+          autoComplete="off"
+          onFocus={() => setUsernameFocus(true)}
+          onBlur={() => setUsernameFocus(false)}
+        />
+        <p
+          className={
+            usernameFocus && username && !validUsername
+              ? "instructions"
+              : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} /> <br />
+          4 to 23 characters.
+          <br />
+          Must begin with a letter.
+          <br />
+          Letter, numbers, hypen, underscore allowed
+          <br />
+        </p>
+
+        <label className="form__label" htmlFor="password" aria-live="assertive">
+          Password
+          <span className={validPassword ? "valid" : "hide"}>
+            {<FontAwesomeIcon icon={faCheck} />}
+          </span>
+          <span className={!validPassword && password ? "invalid" : "hide"}>
+            {<FontAwesomeIcon icon={faTimes} />}
+          </span>
+        </label>
+        <input
+          className="form__input"
+          type="password"
+          id="password"
+          value={password}
+          onChange={onPasswordChange}
+          required
+          onFocus={() => setPasswordFocus(true)}
+          onBlur={() => setPasswordFocus(false)}
+        />
+        <p
+          className={
+            passwordFocus && !validPassword ? "instructions" : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfo} /> <br />
+          8 to 23 characters <br />
+          Must include a uppercase, lowercase letters, a number and special
+          characters <br />
+          Allowed special characters: ! @ # $ %
+        </p>
+
+        <label
+          className="form__label"
+          htmlFor="matchPassword"
+          aria-live="assertive"
+        >
+          Confirm Password
+          <span
+            className={validMatchPassword && validPassword ? "valid" : "hide"}
+          >
+            {<FontAwesomeIcon icon={faCheck} />}
+          </span>
+          <span
+            className={!validMatchPassword && password ? "invalid" : "hide"}
+          >
+            {<FontAwesomeIcon icon={faTimes} />}
+          </span>
+        </label>
+        <input
+          className="form__input"
+          type="password"
+          id="matchPassword"
+          value={matchPassword}
+          onChange={onMatchChange}
+          required
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
+        />
+        <p
+          className={
+            validPassword && matchFocus && !validMatchPassword
+              ? "instructions"
+              : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} /> <br />
+          Must be the same as Password
+        </p>
+
+        <label className="form__roles" htmlFor="roles" aria-live="assertive">
+          Roles
+        </label>
+        <select
+          className="form__input--roles"
+          name="roles"
+          id="roles"
+          multiple={true}
+          value={roles}
+          onChange={onRolesChange}
+          required
+        >
+          {rolesOption}
+        </select>
+
+        <button className="form __button">Add User</button>
+      </form>
+    </section>
+  );
+
+  return content;
+};
+export default NewUserForm;
