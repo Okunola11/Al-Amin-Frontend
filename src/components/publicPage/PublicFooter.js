db1ab85f79578874c523e9a5479e9cfcac0c5440
Abdulwasiu <abdayoade@gmail.com>
2024-01-21 10:14:27 +0100

    Refactored the page's footer

--- src/components/publicPage/PublicFooter.js
+++ src/components/publicPage/PublicFooter.js
@@ -1,9 +1,25 @@
 const PublicFooter = () => {
+  const date = new Date();
+  const today = date.getFullYear();
+
   const content = (
     <footer className="public__footer">
-      <p className="about-footer">
-        &copy; {new Date().getFullYear()} Al Amin School. All rights reserved.
-      </p>
+      <section className="public-footer__section">
+        <address>
+          Al Amin Schools
+          <br />
+          9A Adekunle Adebayo Close <br />
+          Dalemo, Alakuko, LG 12345 <br />
+          <a href="tel: +2345555555555">(234) 555-555-5555</a>
+          <p>Mail: alaminschools@gmail.com</p>
+        </address>
+        <div className="public-footer__div">
+          <p className="about-footer">
+            Copyright &copy; {today} Al Amin School.
+          </p>
+          <p className="about-footer">All rights reserved.</p>
+        </div>
+      </section>
     </footer>
   );
 
