4ffc980cb3b55e488ab72fa2ffd246a0df6f56a8
Abdulwasiu <abdayoade@gmail.com>
2024-01-27 11:19:27 +0100

    Refactored the header to have a better user experience

--- src/components/publicPage/PublicHeader.js
+++ src/components/publicPage/PublicHeader.js
@@ -1,7 +1,41 @@
 import { Link } from "react-router-dom";
 import school_logo from "../../img/school_logo.jpg";
+import { useState, useEffect } from "react";
 
 const PublicHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+      if (
+        !event.target.closest(".nav-button") &&
+        !event.target.closest(".has-subnav")
+      ) {
+        closeMenu();
+      }
+    };
+
+    const handleScroll = () => {
+      closeMenu();
+    };
+
+    if (isOpen) {
+      document.addEventListener("click", handleDocumentClick);
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isOpen]);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const closeMenu = () => setIsOpen(false);
+
+  const activeClass = isOpen ? "active" : "";
+
   const content = (
     <header className="public__header">
       <section className="public-header__section">
@@ -14,7 +48,7 @@ const PublicHeader = () => {
           </h1>
           <p>Empowering Minds, Building Futures</p>
         </div>
-        <button className="nav-button">
+        <button className="nav-button" onClick={toggleMenu}>
           <div className="nav-icon"></div>
         </button>
         <nav className="long-nav">
@@ -49,25 +83,25 @@ const PublicHeader = () => {
         </nav>
       </section>
       <section>
-        <nav className="small-nav">
-          <ul>
-            <li>
+        <nav className={`small-nav ${activeClass}`}>
+          <ul className="small-nav__list">
+            <li onClick={closeMenu}>
               <a href="#home">Home</a>
             </li>
-            <li>
+            <li onClick={closeMenu}>
               <a href="#about">About</a>
             </li>
-            <li>
+            <li onClick={closeMenu}>
               <a href="#admissions">Admissions</a>
             </li>
-            <li>
+            <li onClick={closeMenu}>
               <a href="#programs">Program</a>
             </li>
-            <li>
+            <li onClick={closeMenu}>
               <a href="#contact">Contact</a>
             </li>
             <li className="has-subnav">
-              <a href="#home">Login Portal</a>
+              <Link>Login Portal</Link>
               <ul className="subnav">
                 <li>
                   <Link to="/login">Employee</Link>
