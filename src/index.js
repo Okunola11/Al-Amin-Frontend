024e83ecc70b4944923c5e10dca9d077bb6d31b7
Abdulwasiu <abdayoade@gmail.com>
2024-03-24 17:21:55 +0100

    Changed base url to the deployed api and disabled dev tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 import { store } from "./app/store";
 import { Provider } from "react-redux";
